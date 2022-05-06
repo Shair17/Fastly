14d9c5584de6174fbf445bc3aa6ba7250c7ebc4d
Shair17 <jimmy_jhair17@hotmail.com>
2022-05-06 00:35:45 -0500

    Proyecto de Nextjs base para landing-page de `Fastly`

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+	return <div className="bg-white">hello fastly</div>;
+};
+
+export default Home;
---
+++ web/tailwind.config.js
@@ -0,0 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+	content: ['./src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+				system: defaultTheme.fontFamily.sans,
+			},
+		},
+	},
+	plugins: [],
+};
