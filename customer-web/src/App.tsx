eaf139a8b9a009f445008720d0c76fb429159057
Shair17 <jimmy_jhair17@hotmail.com>
2022-05-14 16:35:28 -0500

    Agregué proyectos base para `customer-web` y `dealer-web`, ahí programaré la landingpage sencilla y el panel administrativo

---
+++ customer-web/src/App.tsx
@@ -0,0 +1,11 @@
+import { MantineProvider } from '@mantine/core';
+
+function App() {
+	return (
+		<MantineProvider withGlobalStyles withNormalizeCSS>
+			<div>customer web</div>
+		</MantineProvider>
+	);
+}
+
+export default App;
---
+++ dealer-web/src/App.tsx
@@ -0,0 +1,11 @@
+import { MantineProvider } from '@mantine/core';
+
+function App() {
+	return (
+		<MantineProvider withGlobalStyles withNormalizeCSS>
+			<div>dealer web</div>
+		</MantineProvider>
+	);
+}
+
+export default App;
