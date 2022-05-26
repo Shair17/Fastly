0b14eaaaf4b644467e1079c2289991f3908be64d
Shair17 <jimmy_jhair17@hotmail.com>
2022-05-26 14:40:43 -0500

    Modifiqué API_URL en app

--- app/src/constants/api.constants.ts
+++ app/src/constants/api.constants.ts
@@ -2,4 +2,4 @@ import {isDev} from './environment.constants';
 
 export const API_BASE = isDev
   ? 'http://192.168.1.46:3000/v1'
-  : 'https://api.fastly.delivery';
+  : 'https://api.fastly.delivery/v1';
