e727cb4299a39c503bd4253344e16ce16aa31c76
Shair17 <jimmy_jhair17@hotmail.com>
2022-07-04 18:19:24 -0500

    Solución a top level await en vitejs

--- web/customer/vite.config.ts
+++ web/customer/vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	build: {
+		minify: 'esbuild',
+		target: 'esnext',
+	},
 	plugins: [react()],
 	publicDir: 'public',
 });
