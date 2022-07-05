e39f8941be5bf32f415699c22c7d900e457626d3
Shair17 <jimmy_jhair17@hotmail.com>
2022-07-05 00:17:33 -0500

    Agregué base para aplicaciones nativas de customer y dealer

--- web/admin/src/constants/api.constants.ts
+++ web/admin/src/constants/api.constants.ts
@@ -1,4 +1,5 @@
-// import { isDev } from '../utils/isDev';
+import { isDev } from '../utils/isDev';
 
-// export const BASE_URL = isDev ? 'http://localhost:3000/v1' : 'https://api.fastly.delivery/v1';
-export const BASE_URL = 'http://localhost:3000/v1';
+export const BASE_URL = isDev
+	? 'http://localhost:3000/v1'
+	: 'https://api.fastly.delivery/v1';
